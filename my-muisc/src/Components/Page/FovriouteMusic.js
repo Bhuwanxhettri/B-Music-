a4bbcd80561596056ccdb338bac31aeb25a68caf
Bhuwanxhettri <bhuwanxhettri12345@gmail.com>
2022-09-17 09:09:41 +0545

    first commmit

---
+++ my-muisc/src/Components/Page/FovriouteMusic.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const FovriouteMusic = () => {
+  return (
+    <div>FovriouteMusic</div>
+  )
+}
+
+export default FovriouteMusic
---
+++ my-muisc/src/Components/Page/MusicInsta/MusicInsta.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const MusicInsta = () => {
+  return (
+    <div >MusicInsta</div>
+  )
+}
+
+export default MusicInsta
